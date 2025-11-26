6e7858a7c814702e83e77f28edfa56aa63f55806
AnsiDK <andreas.dunk@gmail.com>
2025-11-26 13:06:17 +0100

    Add integration test for user timeline and update dependency version to solve a build warning

--- test/IntegrationTests.cs
+++ test/IntegrationTests.cs
@@ -1,7 +1,11 @@
 using System.Net;
+using System.Text.RegularExpressions;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
 using Xunit;
+using Chirp.Infrastructure.Data;
+using Chirp.Core.Entities;
 
 namespace Chirp.IntegratedTests;
 
@@ -12,9 +16,11 @@ namespace Chirp.IntegratedTests;
 public class IntegrationTests : IClassFixture<WebApplicationFactory<Program>>
 {
     private readonly HttpClient _client;
+    private readonly WebApplicationFactory<Program> _factory;
 
     public IntegrationTests(WebApplicationFactory<Program> factory)
     {
+        _factory = factory;
         _client = factory.CreateClient();
     }
 
@@ -36,5 +42,48 @@ public class IntegrationTests : IClassFixture<WebApplicationFactory<Program>>
 
         // Assert
         response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+
+    [Fact]
+    public async Task UserTimeline_FirstPage_ShowsExactly32Cheeps()
+    {
+        // Arrange: seed a test author + 40 cheeps
+        using (var scope = _factory.Services.CreateScope())
+        {
+            var services = scope.ServiceProvider;
+            var db = services.GetRequiredService<ChirpDbContext>();
+
+            // create test author
+            var author = new Author
+            {
+                Name = "integration-test-author",
+                UserName = "integration-test-author",
+                Email = "integration@example.local"
+            };
+            db.Authors.Add(author);
+            db.SaveChanges();
+
+            // create 40 cheeps (newest first)
+            for (int i = 1; i <= 40; i++)
+            {
+                db.Cheeps.Add(new Cheep
+                {
+                    AuthorId = author.Id,
+                    Text = $"cheep {i}",
+                    Timestamp = DateTime.UtcNow.AddMinutes(-i)
+                });
+            }
+            db.SaveChanges();
+        }
+
+        // request the user timeline (author name is used in route)
+        var response = await _client.GetAsync($"/integration-test-author");
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var html = await response.Content.ReadAsStringAsync();
+
+        // Count occurrences of our cheep text pattern on the page.
+        var matches = Regex.Matches(html, @"\bcheep\s*\d+\b", RegexOptions.IgnoreCase);
+        matches.Count.Should().Be(32);
     }
 }
