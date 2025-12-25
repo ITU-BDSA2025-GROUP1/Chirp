0aa1820d26bd9c0142e21f790b1a60f98baa8044
Kolomito <luvp@itu.dk>
2025-12-25 16:29:26 +0100

    Added migrations

---
+++ src/Chirp.Infrastructure/Migrations/20251225125053_AddCheepLikes.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Chirp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCheepLikes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CheepLikes",
+                columns: table => new
+                {
+                    LikedCheepsCheepId = table.Column<int>(type: "INTEGER", nullable: false),
+                    LikesId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CheepLikes", x => new { x.LikedCheepsCheepId, x.LikesId });
+                    table.ForeignKey(
+                        name: "FK_CheepLikes_AspNetUsers_LikesId",
+                        column: x => x.LikesId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CheepLikes_Cheeps_LikedCheepsCheepId",
+                        column: x => x.LikedCheepsCheepId,
+                        principalTable: "Cheeps",
+                        principalColumn: "CheepId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CheepLikes_LikesId",
+                table: "CheepLikes",
+                column: "LikesId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CheepLikes");
+        }
+    }
+}
